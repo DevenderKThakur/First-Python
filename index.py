c77e35fd66db7dbe90eab8cec65e0e55c470568b
DevenderKThakur <101546402+DevenderKThakur@users.noreply.github.com>
2022-10-20 16:45:37 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,17 @@
+                            #    practice python                            
+# print('hello ')                            
+
+# input is a build in function to ask the user about something 
+
+# name = input('what is your name: ')
+
+# its a format string way to display the name with some string 
+
+# print(f"Hello,{name}")
+
+
+n =int(input("Number:"))
+if n%2==0 :
+    print('even number')
+else:
+    print('odd number')    
